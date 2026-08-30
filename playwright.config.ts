20300cb8d55b9a57c6e3d055dbab2652d6b81ea5
PavelAnhur <pavelanhur@gmail.com>
2026-08-30 12:48:19 +0300

    playwright.config.ts fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,7 +46,7 @@ export default defineConfig({
   projects: [
     {
       name: "ui",
-      testDir: "./src/tests/ui",
+      testDir: "./tests/specs/ui",
       use: { ...devices["Desktop Chrome"] },
     },
     {
@@ -84,11 +84,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:4000',
-    reuseExistingServer: !process.env['CI'],
-  },
 });
